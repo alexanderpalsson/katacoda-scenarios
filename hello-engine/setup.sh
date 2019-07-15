4ca4211e7f3b98c03bd91b321ce18d6c0020e751
Alexander Pålsson <fdg@qlik.com>
2019-07-15 11:25:24 +0200

    Updated

--- hello-engine/setup.sh
+++ hello-engine/setup.sh
@@ -1,2 +1,1 @@
-sleep 2; cd ../home/scrapbook/tutorial/example/; ACCEPT_EULA=yes docker-compose up -d
-; npm install; 
+sleep 2; cd ../home/scrapbook/tutorial/example/; ACCEPT_EULA=yes docker-compose up -d; npm install; 
